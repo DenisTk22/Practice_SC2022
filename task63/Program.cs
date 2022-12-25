841c87a4199543a9e5fbeebc3e7126dce0fab44d
Denis Tkachev <allpost-s@ya.ru>
2022-12-25 21:48:50 +0300

    добавлены задачи с семинара 9

---
+++ task63/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+int num = 5;
+int natur (int n)
+{
+    if(n < 0) return -1;
+    if(n == 0 || n == 1) return 1;
+    Console.Write(natur(n-1));
+    return n;
+}
+
+Console.WriteLine(natur(num));
---
+++ task65/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+int num = 4;
+int numup = 8;
+int natur (int n, int m)
+{
+    if(n < 0) return -1;
+    if(n == m) return n;
+    Console.Write(natur(n, m-1));
+    return m;
+}
+
+Console.WriteLine(natur(num, numup));
+
